5c313b5076f8bb7b77bfb28160c83ce04bf241f1
Andrew Huang <andrew.huang12@gmail.com>
2016-06-20 23:40:32 -0700

    Update state with photos fetched from API

--- src/reducers/photoFeed.js
+++ src/reducers/photoFeed.js
@@ -1,9 +1,23 @@
+import { REQUEST_PHOTOS, RECEIVE_PHOTOS } from '../actions/photoFeed';
+
 const initialState = {
   selectedFeed: 'cabin',
   cabinCards: [],
   architectureCards: []
 }
 
-export default function photoFeed(state = initialState, action) {
-  return state;
+export default photoFeed = (state = initialState, action) => {
+  switch (action.type) {
+    case REQUEST_PHOTOS:
+      return state;
+    case RECEIVE_PHOTOS:
+      let cardsArray = `${action.feed}Cards`;
+
+      return {
+        ...state,
+        [cardsArray]: action.photos
+      }
+    default:
+      return state;
+  }
 }
