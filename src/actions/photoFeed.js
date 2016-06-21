69739bc5f9aa32a0f9deb0e84741fb588795e915
Andrew Huang <andrew.huang12@gmail.com>
2016-06-21 16:41:56 -0700

    Add explanatory comments to action creator

--- src/actions/photoFeed.js
+++ src/actions/photoFeed.js
@@ -11,14 +11,15 @@ export const changeFeed = (newFeed) => {
 }
 
 export const fetchPhotosOnFeedChange = (feed) => {
+  // When we switch tabs, we need to reflect that feed change and also fetch the new batch of photos
   return dispatch => {
     dispatch(changeFeed(feed));
-    // Reset timesFetched to 0
-    dispatch(fetchPhotos(feed, 0, 'FeedChange'));
+    dispatch(fetchPhotos(feed, 0, 'FeedChange')); // reset timesFetched to 0
   }
 }
 
 export const fetchPhotosOnEndReached = (feed, timesFetched) => {
+  // When we reach the end of the list, we simply fetch the next 10 photos for the given feed
   return dispatch => {
     dispatch(fetchPhotos(feed, timesFetched, 'EndReached'));
   }
@@ -34,6 +35,7 @@ export const updateDataSource = (feed, photos, feedOrEnd) => {
 }
 
 export const requestPhotos = (feedOrEnd) => {
+  // REQUEST_PHOTOS is dispatched when a fetch is initiated and is responsible for rendering (or hiding) the loading spinner
   return {
     type: REQUEST_PHOTOS,
     feedOrEnd
@@ -41,6 +43,7 @@ export const requestPhotos = (feedOrEnd) => {
 }
 
 export const receivePhotos = (feed, photos, timesFetched) => {
+  // RECEIVE_PHOTOS is dispatched when we've succesfully retrieved a new batch of photos from the API
   return {
     type: RECEIVE_PHOTOS,
     feed,
@@ -51,7 +54,7 @@ export const receivePhotos = (feed, photos, timesFetched) => {
 
 export const fetchPhotos = (feed, timesFetched, feedOrEnd) => {
   return dispatch => {
-    // Dispatch the REQUEST_PHOTOS action
+    // First dispatch REQUEST_PHOTOS to load the spinner
     dispatch(requestPhotos(feedOrEnd));
 
     const apiURLs = {
@@ -59,21 +62,26 @@ export const fetchPhotos = (feed, timesFetched, feedOrEnd) => {
       'architecture': 'https://api.imgur.com/3/gallery/t/architecture/'
     }
 
-    return fetch(apiURLs[feed], {
-      headers: {
-        'Authorization': 'Client-ID 4be88f0185b83d5'
-      }
-    })
+    const url = apiURLs[feed];
+    const clientID = 'Client-ID 4be88f0185b83d5';
+    const headers = {
+      'Authorization': clientID
+    }
+
+    return fetch(url, { headers })
     .then(res => {
       return res.json()
     })
     .then(data => {
       if (data.success) {
         let feedCards = [];
+
+        // Here we parse out the next 10 photos
         let startIndex = timesFetched * 1;
         let endIndex = (timesFetched + 1) * 10;
 
         for (let i = startIndex; i < endIndex; i++) {
+          // Cabin porn and #architecture have slightly different response object structures, so we account for that here
           let photo = (feed === 'cabin')
             ? data.data[i]
             : data.data.items[i]
@@ -84,12 +92,14 @@ export const fetchPhotos = (feed, timesFetched, feedOrEnd) => {
             description: photo.description,
             datetime: photo.datetime,
             views: photo.views,
-            title: photo.title
+            title: photo.title  // many of the photos don't have a description, so we'll render the titles for those guys
           }
           feedCards.push(cardData);
         }
 
+        // Success! RECEIVE_PHOTOS increments timesFetched and stores our new photos in state
         dispatch(receivePhotos(feed, feedCards, timesFetched));
+        // UPDATE_DATA_SOURCE will trigger a re-render of our UI
         dispatch(updateDataSource(feed, feedCards, feedOrEnd));
       } else {
         console.log('Uh oh, something went wrong! Got status code ' + res.status);
